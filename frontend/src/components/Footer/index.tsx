2d19c7aeed0ac11696ccb23348cbba7cde11ead3
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-05-05 10:54:36 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/BrunoRodriguesN" target="_blank" rel="noreferrer">Bruno Rodrigues</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/brunoorodr" target="_blank" rel="noreferrer">@brunoorodr</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
